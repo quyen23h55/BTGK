86d6ed05b07f75339b37b79ae480818b39bb858f
Quyen <luonglpck8@gmail.com>
2016-05-06 16:15:06 +0700

    Cho phép sửa đổi, bổ sung thông tin mục Summary và Experience

--- public_html/javascript.js
+++ public_html/javascript.js
@@ -1,17 +1,21 @@
 var app = angular.module('myApp', []);
             app.controller('customersCtrl', function($scope, $http) {
               $http.get("myData.json").then(function (response) {
-                  $scope.data = response.data;
-                  $scope.data.experience.push({logo:"img/linkedin.png",title:$scope.name,subtitle:"LinkedIn",time:"April 2016"});                  
+                  $scope.data = response.data;                  
                   $scope.name = $scope.data.profile.name;
                   $scope.headline = $scope.data.profile.headline;
                   $scope.region = $scope.data.profile.region;
                   $scope.fb = $scope.data.profile.facebook;
                   $scope.gmail = $scope.data.profile.gmail;
                   $scope.phone = $scope.data.profile.phone;
-                  $scope.description1 = $scope.data.sumary.description1;
+                  $scope.description = $scope.data.sumary.description;
                   $scope.title = "";
                   $scope.content = "";
+                  $scope.addresume = "";
+                  $scope.explogo = "";
+                  $scope.exptitle = "";
+                  $scope.expsubtitle = "";
+                  $scope.exptime = "";
                   
                   $scope.display = false;
                   $scope.show = true;
@@ -25,11 +29,17 @@ var app = angular.module('myApp', []);
                   $scope.show_gmail = true;
                   $scope.display_phone = false;
                   $scope.show_phone = true;
-                  $scope.display_description1 = false;
-                  $scope.show_description1 = true;
+                  
+                  $scope.display_description = false;
+                  $scope.show_description = true;
+                  
                   $scope.display_plush = false;
                   $scope.show_plush = true;
                   $scope.show_title = false;
+                  
+                  $scope.display_resume = false;
+                  $scope.display_experience = false;
+                  $scope.display_addexperience = false;
               });
               $scope.save = function(){
                   $scope.data.profile.name = $scope.name;
@@ -93,15 +103,22 @@ var app = angular.module('myApp', []);
                   $scope.display_phone = false;
                   $scope.show_phone = true;
             }
-            $scope.savedescription1 = function(){
-                  $scope.data.sumary.description1 = $scope.description1;
-                  $scope.display_description1 = false;
-                  $scope.show_description1 = true;
-}
-            $scope.canceldescription1 = function(){
-                  $scope.description1 = $scope.data.sumary.description1;
-                  $scope.display_description1 = false;
-                  $scope.show_description1 = true;                 
+            $scope.savedescription = function(index){
+                  $scope.data.sumary[index].description = $scope.test;
+                  $scope.display_description = false;
+                  $scope.show_description = true;
+            }
+            $scope.canceldescription = function(){
+                  $scope.description = $scope.data.sumary.description;
+                  $scope.display_description = false;
+                  $scope.show_description = true;  
+            }
+            $scope.clicksummary = function(index){
+              $scope.display_description = true;
+              $scope.show_description = true;;
+              $scope.test = $scope.data.sumary[index].description;
+              $scope.i = index;
+              
             }
             $scope.saveplush= function(){
                   $scope.data.profileplush.push({title:$scope.title,content:$scope.content});
@@ -117,5 +134,45 @@ var app = angular.module('myApp', []);
                   $scope.title = "";
                   $scope.content = "";  
                   $scope.show_title = false;
+            }
+            $scope.saveaddresume = function(){
+                $scope.data.sumary.push({description:$scope.addresume});
+                $scope.display_addresume = false;
+                $scope.addresume = "";
+            }
+            $scope.canceladdresume = function(){
+                $scope.addresume = "";
+                $scope.display_addresume = false;
+            }
+            $scope.clickexperience = function(index){
+                $scope.display_experience = true;
+                $scope.explogo = $scope.data.experience[index].logo;
+                $scope.exptitle = $scope.data.experience[index].title;
+                $scope.expsubtitle = $scope.data.experience[index].subtitle;
+                $scope.exptime = $scope.data.experience[index].time;
+                $scope.iexp = index;
+            }
+            $scope.saveexperience = function(index){
+                //$scope.data.experience.push({logo:$scope.explogo,title:$scope.exptitle,subtitle:$scope.exptime});
+                $scope.display_experience = false;
+                $scope.data.experience[index].logo = $scope.explogo;
+                $scope.data.experience[index].title = $scope.exptitle;
+                $scope.data.experience[index].subtitle = $scope.expsubtitle;
+                $scope.data.experience[index].time = $scope.exptime;
+            }
+            $scope.cancelexperience = function(){
+                $scope.display_experience = false;
+                $scope.explogo = "";
+                $scope.exptitle = "";
+                $scope.expsubtitle = "";
+                $scope.exptime = "";
+            }
+            $scope.saveaddexperience = function(){
+                $scope.data.experience.push({logo:$scope.explogo,title:$scope.exptitle,subtitle:$scope.exptime});
+                $scope.display_addexperience = false;
+                $scope.explogo = "";
+                $scope.exptitle = "";
+                $scope.expsubtitle = "";
+                $scope.exptime = "";
             }
             });
